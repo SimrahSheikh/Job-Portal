613186f8b73c3cfa5d07d8f6d00a55921f5acaeb
Palkesh Sharma <58721387+Palkesh-ai@users.noreply.github.com>
2025-02-25 18:18:19 +0530

    Integrate Redux for state management and set up persistence

---
+++ src/store/store.jsx
@@ -0,0 +1,27 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage 
+import rootReducer from "./rootReducer";
+const persistConfig = {
+
+    key: 'root',
+  
+    storage,
+  
+  };
+  
+  
+  const persistedReducer = persistReducer(persistConfig, rootReducer);
+  
+  
+  const store = configureStore({
+  
+    reducer: persistedReducer,
+  
+  });
+  
+  
+  const persistor = persistStore(store);
+  
+  
+  export { store, persistor };
