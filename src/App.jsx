4cc3c30f9a9e3b3463f8538ef684bf2bbeecd46f
SimrahSheikh <simrahsheikh18@gmail.com>
2025-02-26 12:21:09 +0530

    updated Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-// import './App.css'
 import Login from "./components/Login";
 import { BrowserRouter } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
-import SignUp from "./components/SignUp";
+import Signup from "./components/Signup";
+import  NavBar  from "./components/HR_Components/NavBar";
+import  PostJob  from "./components/HR_Components/PostJob";
+import HRLayout from "./components/Layouts/HRLayout";
+import { Applications } from "./components/HR_Components/Applications";
+import PostedJobs from "./components/HR_Components/PostedJobs";
 
 function App() {
   return (
-    <>
+    <div className="relative">
       <BrowserRouter>
+    
         <Routes>
+          
           <Route path="/" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+
+          
+
+        <Route path="/hr" element={<HRLayout />}>
+          <Route path="/hr/postjob" element={<PostJob />} />
+          <Route path="/hr/applications" element={<Applications />} /> 
+          <Route path ="/hr/postedjobs" element={<PostedJobs/>}/>
+        </Route>
+
         </Routes>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
 export default App;
+
