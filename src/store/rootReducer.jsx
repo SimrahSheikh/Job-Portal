70c234b209af35b319575bdedd1e1e2bf4fd0444
Palkesh Sharma <58721387+Palkesh-ai@users.noreply.github.com>
2025-03-12 13:33:38 +0530

    Implement Redux for job fetching and update JobList component

--- src/store/rootReducer.jsx
+++ src/store/rootReducer.jsx
@@ -1,35 +1,11 @@
 import { combineReducers } from "redux";
 import authReducer from "./slice/AuthSlice"
-
-// // Example reducer for user authentication
-// const authReducer = (state = { isAuthenticated: false }, action) => {
-//   switch (action.type) {
-//     case "LOGIN":
-//       return { ...state, isAuthenticated: true };
-//     case "LOGOUT":
-//       return { ...state, isAuthenticated: false };
-//     default:
-//       return state;
-//   }
-// };
-
-// // Example reducer for job listings
-// const jobReducer = (state = { jobs: [] }, action) => {
-//   switch (action.type) {
-//     case "ADD_JOB":
-//       return { ...state, jobs: [...state.jobs, action.payload] };
-//     case "REMOVE_JOB":
-//       return { ...state, jobs: state.jobs.filter(job => job.id !== action.payload.id) };
-//     default:
-//       return state;
-//   }
-// };
-
-// Combine reducers
+import postReducer from "./slice/userSlice/postSlice"
 
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  post: postReducer
 });
 
 export default rootReducer;
