317ecbcdbd766c2fc36ee5fda831ddc0c77ac465
ananyo-victor <ananyomaitroan@gmail.com>
2025-03-13 18:22:31 +0530

    Add Premium route and enhance UserNavBar with Log Out option; update Popup component for better user feedback

--- src/components/User_Components/UserNavBar.jsx
+++ src/components/User_Components/UserNavBar.jsx
@@ -1,24 +1,28 @@
 import { Link, useLocation } from "react-router-dom";
-import { Briefcase, Bookmark, FileText, User } from "lucide-react";
+import { Briefcase, Bookmark, FileText, User, LogOut } from "lucide-react";
 
 const UserNavBar = () => {
   const location = useLocation();
   // console.log(location.pathname);
-  
+
 
   return (
     <aside className="h-full w-64 border-r shadow-md flex flex-col p-4">
       <div className="flex items-center justify-center py-4 text-xl font-bold text-gray-700">
         Job Portal
       </div>
- 
-      <nav className="flex-1">
+
+      <nav className="flex flex-col justify-between h-full">
         <ul className="space-y-2">
           <NavItem icon={<Briefcase size={20} />} text="Jobs" link="/user/jobs" currentPath={location.pathname} />
           <NavItem icon={<FileText size={20} />} text="Applied Jobs" link="/user/applied-jobs" currentPath={location.pathname} />
           <NavItem icon={<Bookmark size={20} />} text="Saved Jobs" link="/user/saved-jobs" currentPath={location.pathname} />
           <NavItem icon={<User size={20} />} text="Profile" link="/user/profile" currentPath={location.pathname} />
         </ul>
+        <Link to={"/login"} className={`flex items-center p-3 rounded-md transition-all focus:outline-none focus:ring-2 focus:ring-indigo-200`}>
+          {<LogOut size={20} />}
+          <span className="ml-3 text-gray-700 font-medium">Log Out</span>
+        </Link>
       </nav>
     </aside>
   );
